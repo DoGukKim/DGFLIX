cbd07d09ea02d26170f7a0f6c3bf94358c512a50
DOGUK <dgkim1994@gmail.com>
2020-08-18 15:37:55 +0900

    Updates

---
+++ precache-manifest.ede8a17a930a6b8d574da3432231a5f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d69cccdcfdc3bc338303ccde9ee8225",
+    "url": "/dgflix/index.html"
+  },
+  {
+    "revision": "f76bfae132a290887adc",
+    "url": "/dgflix/static/js/2.48706a47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dgflix/static/js/2.48706a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63aa3fe03476313ae983",
+    "url": "/dgflix/static/js/main.216bfd68.chunk.js"
+  },
+  {
+    "revision": "8a5ac5e0d81cc1ce5421",
+    "url": "/dgflix/static/js/runtime-main.e4c0d6e0.js"
+  }
+]);
